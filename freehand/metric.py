bc27e585f179559848faa63f5a54fb4a786ef2e9
Qi <qi.li.21@ucl.ac.uk>
2022-11-08 14:49:10 +0000

    ref #2 overlap metric updated

--- freehand/metric.py
+++ freehand/metric.py
@@ -34,10 +34,28 @@ def iterate_frames(pts, px):
     for idx in range(pts.shape[2]-1):
         ps_hex = np.concatenate((pts[...,idx],pts[...,idx+1]), axis=1) 
         mask = mask | test_inside_hexahedron(px, ps_hex)
+        
+
+        # debug:
+        # is_inside = test_inside_hexahedron(px, ps_hex)
+        # print(np.count_nonzero(is_inside))
+        # px_inside = px[np.ix_([True,True,True],is_inside)]
+        #
+        # import matplotlib.pyplot as plt
+        # fig = plt.figure()
+        # ax = fig.add_subplot(projection='3d')
+        # ax.scatter(ps_hex[0,:], ps_hex[1,:], ps_hex[2,:], marker='x')
+        # ax.scatter(px_inside[0,:], px_inside[1,:], px_inside[2,:], marker='.')
+        # plt.show()
+        #
+        # print(idx)
+
+
     return mask
 
 
-def test_inside_hexahedron(px, ps_hex):
+
+def test_inside_hexahedron(px, ps_hex, three_vert=True):
 
     '''
     Algorithm described in https://stackoverflow.com/questions/72170319/check-if-a-point-is-inside-an-arbitrary-hexahedron
@@ -45,24 +63,56 @@ def test_inside_hexahedron(px, ps_hex):
     :px 3-by-n point vectors to test
     :ps 3-by-8 point vectors representing hexahedron conners 
         node 0-3 and node 4-7 are in the same (anti-)clockwise order
+    
+                  t
+                  |
+         4--------|-------------7
+        /|        |            /|
+       / |        |           / |
+      /  |        |          /  |
+     /   |        |         /   |
+    /    |        |        /    |
+   /     |        |       /     |
+  5----------------------6      |
+  |      |        |      |      |
+  |      |        o------|---------s
+  |      |       /       |      |
+  |      0------/--------|------3
+  |     /      /         |     /
+  |    /      /          |    /
+  |   /      /           |   /
+  |  /      /            |  /
+  | /      r             | /
+  |/                     |/
+  1----------------------2
+
     '''
 
     # compute the normal vectors 
-    r_pos_normvec = np.cross(ps_hex[:,2]-ps_hex[:,6], ps_hex[:,5]-ps_hex[:,6])
-    r_neg_normvec = np.cross(ps_hex[:,4]-ps_hex[:,0], ps_hex[:,3]-ps_hex[:,0])
-    s_pos_normvec = np.cross(ps_hex[:,2]-ps_hex[:,6], ps_hex[:,7]-ps_hex[:,6])
-    s_neg_normvec = np.cross(ps_hex[:,4]-ps_hex[:,0], ps_hex[:,1]-ps_hex[:,0])
-    t_pos_normvec = np.cross(ps_hex[:,5]-ps_hex[:,6], ps_hex[:,7]-ps_hex[:,6])
-    t_neg_normvec = np.cross(ps_hex[:,1]-ps_hex[:,0], ps_hex[:,3]-ps_hex[:,0])
-    # for opposite nodes 0 and 6:
+    if three_vert:  # normal vector by three vertices 
+        r_pos_nv = np.cross(ps_hex[:,2]-ps_hex[:,1], ps_hex[:,5]-ps_hex[:,1])
+        r_neg_nv = np.cross(ps_hex[:,4]-ps_hex[:,0], ps_hex[:,3]-ps_hex[:,0])
+        s_pos_nv = np.cross(ps_hex[:,3]-ps_hex[:,2], ps_hex[:,6]-ps_hex[:,2])
+        s_neg_nv = np.cross(ps_hex[:,5]-ps_hex[:,1], ps_hex[:,0]-ps_hex[:,1])
+        t_pos_nv = np.cross(ps_hex[:,6]-ps_hex[:,5], ps_hex[:,4]-ps_hex[:,5])
+        t_neg_nv = np.cross(ps_hex[:,0]-ps_hex[:,1], ps_hex[:,2]-ps_hex[:,1])
+    else:  # normal vector by four vertices 
+        r_pos_nv = np.cross(ps_hex[:,6]-ps_hex[:,1], ps_hex[:,5]-ps_hex[:,2])
+        r_neg_nv = np.cross(ps_hex[:,7]-ps_hex[:,0], ps_hex[:,4]-ps_hex[:,3])
+        s_pos_nv = np.cross(ps_hex[:,7]-ps_hex[:,2], ps_hex[:,6]-ps_hex[:,3])
+        s_neg_nv = np.cross(ps_hex[:,4]-ps_hex[:,1], ps_hex[:,5]-ps_hex[:,0])
+        t_pos_nv = np.cross(ps_hex[:,7]-ps_hex[:,5], ps_hex[:,4]-ps_hex[:,6])
+        t_neg_nv = np.cross(ps_hex[:,3]-ps_hex[:,1], ps_hex[:,0]-ps_hex[:,2])
+    
+    # for opposite nodes 0 (negative) and 6 (positive):
     p0x = (px-ps_hex[:,[0]]).transpose()
     p6x = (px-ps_hex[:,[6]]).transpose()
     is_inside = np.all(np.stack([
-        np.inner(p0x,r_neg_normvec)<0, 
-        np.inner(p0x,s_neg_normvec)<0, 
-        np.inner(p0x,t_neg_normvec)<0, 
-        np.inner(p6x,r_pos_normvec)<0, 
-        np.inner(p6x,s_pos_normvec)<0, 
-        np.inner(p6x,t_pos_normvec)<0],axis=1), axis=1)
+        np.inner(p0x,r_neg_nv)<0, 
+        np.inner(p0x,s_neg_nv)<0, 
+        np.inner(p0x,t_neg_nv)<0, 
+        np.inner(p6x,r_pos_nv)<0, 
+        np.inner(p6x,s_pos_nv)<0, 
+        np.inner(p6x,t_pos_nv)<0],axis=1), axis=1)
     # np.count_nonzero(is_inside)
     return is_inside
